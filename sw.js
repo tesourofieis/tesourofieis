7ca17850249eca5d40330a58b39ac1c161d95bec
ofrades <mig.silva@gmail.com>
2019-12-26 22:18:00 +0000

    pwa and deleted files

--- sw.js
+++ sw.js
@@ -1,10 +1,31 @@
+importScripts(
+  "https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js"
+);
+importScripts(
+  "https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js"
+);
+importScripts(
+  "https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js"
+);
+
+const missaRoute = new workbox.routing.RegExpRoute({
+  regExp: new RegExp("missa/*"),
+  handler: new workbox.runtimeCaching.CacheFirst()
+});
+
+const router = new workbox.routing.Router();
+//router.addFetchListener();
+router.registerRoutes({ routes: [missaRoute] });
+router.setDefaultHandler({
+  handler: new workbox.runtimeCaching.CacheFirst()
+});
+
 const cacheName = "site-static-v1";
 
 const staticAssets = [
   "/",
   "/index",
   "/missa",
-  "/missa/",
   "/rosario",
   "/oficio",
   "/antifonas",
