04cad287a686beccf3e39fda6c1c41d296e6e191
ofrades <mig.silva@gmail.com>
2019-12-26 13:06:55 +0000

    pwa

---
+++ sw.js
@@ -0,0 +1,46 @@
+const cacheName = "site-static-v1";
+const staticAssets = [
+  "/",
+  "/index.html",
+  "/missa",
+  "/salterio",
+  "./app.js",
+  "./missa.html"
+];
+
+self.addEventListener("install", async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+
+  if (/.*(json)$/.test(req.url)) {
+    event.respondWith(networkFirst(req));
+  } else {
+    event.respondWith(cacheFirst(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || networkFirst(req);
+}
+
+async function networkFirst(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse;
+  }
+}
