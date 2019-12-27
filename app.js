9b5c637b992f74a8b3378fade7bebafd93600641
ofrades <mig.silva@gmail.com>
2019-12-27 10:49:09 +0000

    comemoracao e oitavario

--- app.js
+++ app.js
@@ -4,3 +4,14 @@
 //     .then(reg => console.log("service worker registered", reg))
 //     .catch(err => console.log("service worker not registerd", err));
 // }
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function () {
+		navigator.serviceWorker.register('/sw.js').then(function (registration) {
+			// Registration was successful
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}, function (err) {
+			// registration failed :(
+			console.log('ServiceWorker registration failed: ', err);
+		});
+	});
+}
