7f491927ee536b124910d2b8d8ecb4b59e1636bf
ofrades <mig.silva@gmail.com>
2019-12-28 16:48:38 +0000

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const cacheName = "site-static-v1";
-
 let today = new Date().toISOString().slice(0, 10);
 
 const staticAssets = [
@@ -31,11 +29,25 @@ const staticAssets = [
   "/adoracao",
   "/ordinariomissa",
   "/oracoesleoninas",
-  "/salterio"
+  "/salterio",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+  "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+  "https://fonts.googleapis.com/css?family=Cardo&display=swap",
+  "/assets/style.css",
+  "/assets/script.js",
+  "/assets/missa.css",
+  "/assets/floating-labels.css",
+  "/app.js"
 ];
 
 self.addEventListener("install", async event => {
-  const cache = await caches.open(cacheName);
+  const cache = await caches.open("v1");
   await cache.addAll(staticAssets);
 });
 
@@ -44,29 +56,16 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  const req = event.request;
-
-  if (/.*(json)$/.test(req.url)) {
-    event.respondWith(networkFirst(req));
-  } else {
-    event.respondWith(cacheFirst(req));
-  }
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return (
+        resp ||
+        fetch(event.request).then(async response => {
+          const cache = await caches.open("v1");
+          cache.put(event.request, response.clone());
+          return response;
+        })
+      );
+    })
+  );
 });
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match(req);
-  return cachedResponse || networkFirst(req);
-}
-
-async function networkFirst(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cachedResponse = await cache.match(req);
-    return cachedResponse;
-  }
-}
