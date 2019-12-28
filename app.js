596c6aea54d081a122443b359573789b8d0246d7
ofrades <mig.silva@gmail.com>
2019-12-28 17:17:27 +0000

    sw

--- app.js
+++ app.js
@@ -4,14 +4,3 @@ if ("serviceWorker" in navigator) {
     .then(reg => console.log("service worker registered", reg))
     .catch(err => console.log("service worker not registerd", err));
 }
-// if ('serviceWorker' in navigator) {
-// 	window.addEventListener('load', function () {
-// 		navigator.serviceWorker.register('/sw.js').then(function (registration) {
-// 			// Registration was successful
-// 			console.log('ServiceWorker registration successful with scope: ', registration.scope);
-// 		}, function (err) {
-// 			// registration failed :(
-// 			console.log('ServiceWorker registration failed: ', err);
-// 		});
-// 	});
-// }
