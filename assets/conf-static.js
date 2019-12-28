f2d039814800a53ca1228835fde37f35b184e462
ofrades <mig.silva@gmail.com>
2019-12-28 22:55:35 +0000

    data

--- assets/conf-static.js
+++ assets/conf-static.js
@@ -3,8 +3,8 @@
  * currently the data files are generated for just three years hence narrow date scope
  **/
 let config = {
-  calendarEndpoint: "missa/data/",
-  dateEndpoint: "missa/data/",
+  calendarEndpoint: "data/",
+  dateEndpoint: "data/",
   minDate: "2018-01-01",
   maxDate: "2020-12-31"
 };
