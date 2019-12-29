5cd45b0e4138ef4af9fcdc8a05ef5d0d1852300c
ofrades <mig.silva@gmail.com>
2019-12-29 00:33:46 +0000

    sw

--- sw.js
+++ sw.js
@@ -112,78 +112,78 @@ workbox.precaching.precacheAndRoute([
   "./assets/conf-static.js"
 ]);
 
-const cacheName = "tesouro-v1";
+// const cacheName = "tesouro-v1";
 
-const staticAssets = [
-  //   "./",
-  //   "./index",
-  //   "./missa",
-  //   "./missa/data/2019",
-  //   "./missa/data/2020",
-  //   `./missa/data/${today}`,
-  //   "./oracaomanha",
-  //   "./oracaonoite",
-  //   "./requiem",
-  //   "./baptismo",
-  //   "./confirmacao",
-  //   "./matrimonio",
-  //   "./extremauncao",
-  //   "./penitencia",
-  //   "./propriotempo",
-  //   "./propriosantos",
-  //   "./comumsantos",
-  //   "./oracoesdiversas",
-  //   "./oracaoantescomunhao",
-  //   "./oracaodepoiscomunhao",
-  //   "./oracoescomplementares",
-  //   "./angelus",
-  //   "./rosario",
-  //   "./oficio",
-  //   "./antifonas",
-  //   "./adoracao",
-  //   "./ordinariomissa",
-  //   "./oracoesleoninas",
-  //   "./salterio",
-  "./assets/style.css",
-  "./assets/script.js",
-  "./assets/missa.css",
-  "./assets/floating-labels.css",
-  "./",
-  "./app.js",
-  "./assets/",
-  "./assets/common.js",
-  "./assets/readings.js",
-  "./assets/conf-static.js",
-  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
-  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-  "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-  "https://fonts.googleapis.com/css?family=Cardo&display=swap",
-  "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.js"
-  //   "./app.js"
-];
+// const staticAssets = [
+//   //   "./",
+//   //   "./index",
+//   //   "./missa",
+//   //   "./missa/data/2019",
+//   //   "./missa/data/2020",
+//   //   `./missa/data/${today}`,
+//   //   "./oracaomanha",
+//   //   "./oracaonoite",
+//   //   "./requiem",
+//   //   "./baptismo",
+//   //   "./confirmacao",
+//   //   "./matrimonio",
+//   //   "./extremauncao",
+//   //   "./penitencia",
+//   //   "./propriotempo",
+//   //   "./propriosantos",
+//   //   "./comumsantos",
+//   //   "./oracoesdiversas",
+//   //   "./oracaoantescomunhao",
+//   //   "./oracaodepoiscomunhao",
+//   //   "./oracoescomplementares",
+//   //   "./angelus",
+//   //   "./rosario",
+//   //   "./oficio",
+//   //   "./antifonas",
+//   //   "./adoracao",
+//   //   "./ordinariomissa",
+//   //   "./oracoesleoninas",
+//   //   "./salterio",
+//   "./assets/style.css",
+//   "./assets/script.js",
+//   "./assets/missa.css",
+//   "./assets/floating-labels.css",
+//   "./",
+//   "./app.js",
+//   "./assets/",
+//   "./assets/common.js",
+//   "./assets/readings.js",
+//   "./assets/conf-static.js",
+//   "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+//   "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+//   "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+//   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
+//   "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css",
+//   "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js",
+//   "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js",
+//   "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+//   "https://fonts.googleapis.com/css?family=Cardo&display=swap",
+//   "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.js",
+//   "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.js"
+//   //   "./app.js"
+// ];
 
-self.addEventListener("install", event => {
-  const cache = caches.open(cacheName);
-  cache.addAll(staticAssets);
-});
+// self.addEventListener("install", async event => {
+//   const cache = await caches.open(cacheName);
+//   await cache.addAll(staticAssets);
+// });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
+// self.addEventListener("activate", event => {
+//   event.waitUntil(self.clients.claim());
+// });
 
-self.addEventListener("fetch", event => {
-  const req = event.request;
-  event.respondWith(cacheFirst(req));
-});
+// self.addEventListener("fetch", event => {
+//   const req = event.request;
+//   event.respondWith(cacheFirst(req));
+// });
 
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match(req);
-  return cachedResponse || fetch(req);
-}
+// async function cacheFirst(req) {
+//   const cache = await caches.open(cacheName);
+//   const cachedResponse = await cache.match(req);
+//   return cachedResponse || fetch(req);
+// }
