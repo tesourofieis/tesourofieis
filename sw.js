dc35763f5f9bda1f5820c7c28f08e1747a1ad101
ofrades <mig.silva@gmail.com>
2020-03-02 18:00:08 +0000

    holy week rosary

--- sw.js
+++ sw.js
@@ -49,24 +49,6 @@ let tp15 = moment(new Date())
   .add(15, "days")
   .format("YYYY-MM-DD");
 
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   "./",
   "./app.js",
@@ -138,21 +120,8 @@ const PRECACHE_URLS = [
   "./img/1.gif"
 ];
 
-const PRECACHE = "precache-v0.0.3";
+const PRECACHE = "precache-v0.0.4";
 const RUNTIME = "runtime";
-
-// The install handler takes care of precaching the resources we always need.
-// self.addEventListener("install", event => {
-//   console.log("The service worker is being installed.");
-
-//   event.waitUntil(
-//     caches
-//       .open(PRECACHE)
-//       .then(cache => cache.addAll(PRECACHE_URLS))
-//       .then(() => console.log("Assets added to cache"))
-//       .then(self.skipWaiting())
-//   );
-// });
 
 self.addEventListener("install", event => {
   event.waitUntil(
@@ -160,7 +129,6 @@ self.addEventListener("install", event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
   console.log("The service worker is serving the asset.");
 
@@ -191,9 +159,6 @@ self.addEventListener("message", function(event) {
   }
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
