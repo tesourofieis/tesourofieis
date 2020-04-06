18cf1d6e25bb69acb49f6aea50de890bf995756a
ofrades <mig.silva@gmail.com>
2020-04-07 00:49:16 +0100

    Pascoa

--- sw.js
+++ sw.js
@@ -88,7 +88,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.1.6";
+const PRECACHE = "precache-v0.1.7";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
