859c69ed9670f1a837369da47d5db52d3ac233c0
ofrades <mig.silva@gmail.com>
2020-04-08 14:55:57 +0100

    add facebook page

--- sw.js
+++ sw.js
@@ -88,7 +88,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.1.8";
+const PRECACHE = "precache-v0.1.9";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
