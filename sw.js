be5a3f2634fe751e4a87a9bc7250d262cc885613
ofrades <mig.silva@gmail.com>
2020-04-08 02:01:49 +0100

    pent sexta-feira santa

--- sw.js
+++ sw.js
@@ -88,7 +88,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.1.7";
+const PRECACHE = "precache-v0.1.8";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
