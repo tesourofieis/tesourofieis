56c566dec86294138b42028a18271ac918473743
ofrades <mig.silva@gmail.com>
2020-04-11 12:04:38 +0100

    novas descricoes

--- sw.js
+++ sw.js
@@ -88,7 +88,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.2.0";
+const PRECACHE = "precache-v0.2.1";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
