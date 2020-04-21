74b2e96c6eb58dfff66b03fb35307d17587c184f
ofrades <mig.silva@gmail.com>
2020-04-21 12:57:34 +0100

    add votivas

--- sw.js
+++ sw.js
@@ -91,7 +91,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.2.9";
+const PRECACHE = "precache-v0.3.0";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
