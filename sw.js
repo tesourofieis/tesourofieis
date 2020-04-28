e6e2f04f02b29023951002de6523cedd5150f415
ofrades <mig.silva@gmail.com>
2020-04-28 17:07:54 +0100

    jose errata

--- sw.js
+++ sw.js
@@ -91,7 +91,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.3.5";
+const PRECACHE = "precache-v0.3.6";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
