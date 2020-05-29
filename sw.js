7575e644707abe24b20084eba8c305d8d40e8a4e
ofrades <mig.silva@gmail.com>
2020-05-29 15:04:22 +0100

    bump version

--- sw.js
+++ sw.js
@@ -91,7 +91,7 @@ const PRECACHE_URLS = [
   "./img/1.gif",
 ];
 
-const PRECACHE = "precache-v0.3.9";
+const PRECACHE = "precache-v0.4.0";
 const RUNTIME = "runtime";
 
 self.addEventListener("install", (event) => {
