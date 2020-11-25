03a5d1e6f195d3ec412b748b7ac773ef0325a667
miguel/ofrades <mig.silva@gmail.com>
2020-11-25 17:52:08 +0000

    feat: styled components setup

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from './styles'
+
+const Header: React.FC = () => {
+  return <Container>My Nav</Container>
+}
+
+export default Header
---
+++ app/styles/global.ts
@@ -0,0 +1,15 @@
+import { GlobalStyleComponent } from 'styled-components'
+
+export default GlobalStyleComponent`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    background: #F5F5F5;
+    font-size: 14px;
+    color: #333;
+    font-family: sans-serif;
+  }
+`
