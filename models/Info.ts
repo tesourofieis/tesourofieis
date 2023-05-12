c927c191dbe3f2a53294d1d287ea50f92bc97a18
ofrades <ofrades@pm.me>
2023-05-12 01:25:42 +0100

    missa first pass

---
+++ models/Info.ts
@@ -0,0 +1,11 @@
+export default interface Info {
+	additional_info: string[];
+	colors: string[];
+	date: Date;
+	description: string;
+	id: string;
+	rank: number;
+	supplements: any[];
+	tempora: null;
+	title: string;
+}
---
+++ models/Section.ts
@@ -0,0 +1,5 @@
+export default interface Section {
+  body: Array<string[]>
+  id: string
+  label: string
+}
