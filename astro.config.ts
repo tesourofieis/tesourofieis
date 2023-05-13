02680bb5826a6f17fb15ae52da02b779b77b2a39
ofrades <ofrades@pop-os.localdomain>
2023-05-13 10:06:26 +0100

    assets to static folder

--- astro.config.ts
+++ astro.config.ts
@@ -4,23 +4,32 @@ import { defineConfig } from 'astro/config';
 import AutoImport from 'astro-auto-import';
 import { asideAutoImport, astroAsides } from './integrations/astro-asides';
 import { theme } from './syntax-highlighting-theme';
-import tailwind from "@astrojs/tailwind"; // https://astro.build/config
-
+import tailwind from '@astrojs/tailwind'; // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://docs.astro.build/',
-  integrations: [AutoImport({
-    imports: [asideAutoImport]
-  }), preact({
-    compat: true
-  }), astroAsides(), mdx(), tailwind()],
-  markdown: {
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme
-    },
-    // Override with our own config
-    smartypants: false
-  }
+	output: 'static',
+	build: {
+		assets: 'static',
+	},
+	site: 'https://tesourofieis.com/',
+	integrations: [
+		AutoImport({
+			imports: [asideAutoImport],
+		}),
+		preact({
+			compat: true,
+		}),
+		astroAsides(),
+		mdx(),
+		tailwind(),
+	],
+	markdown: {
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			theme,
+		},
+		// Override with our own config
+		smartypants: false,
+	},
 });
