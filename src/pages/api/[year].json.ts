e98cd671e043436417ed8cbd39f1a91cae885368
ofrades <ofrades@pm.me>
2024-01-23 23:40:46 +0000

    remove api

--- src/pages/api/[year].json.ts
+++ src/pages/api/[year].json.ts
@@ -1,18 +1,18 @@
-import { getCalendar } from "../../../lib/getCalendar";
-
-export async function GET({ params }) {
-  const year = params.year;
-
-  const calendar = getCalendar(year);
-
-  return new Response(
-    JSON.stringify({
-      id: year,
-      body: calendar,
-    }),
-  );
-}
-
-export function getStaticPaths() {
-  return [{ params: { year: new Date().getFullYear().toString() } }];
-}
+// import { getCalendar } from "../../../lib/getCalendar";
+//
+// export async function GET({ params }) {
+//   const year = params.year;
+//
+//   const calendar = getCalendar(year);
+//
+//   return new Response(
+//     JSON.stringify({
+//       id: year,
+//       body: calendar,
+//     }),
+//   );
+// }
+//
+// export function getStaticPaths() {
+//   return [{ params: { year: new Date().getFullYear().toString() } }];
+// }
