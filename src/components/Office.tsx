bdd98664d73271a7ffc95a5db270272b872caca1
ofrades <ofrades@pm.me>
2024-01-24 17:15:23 +0000

    add interactivity

--- src/components/Office.tsx
+++ src/components/Office.tsx
@@ -1,66 +1,87 @@
 import { getHours } from "date-fns";
+import LinkCard from "./LinkCard";
 
 export default function Office() {
   const date = new Date();
   const hour = getHours(date);
-  const title = "Pequeno Ofício de Nossa Senhora";
-  return (
-    <>
-      {hour >= 0 && hour < 3 && (
-        <a href="/devocionario/oficio/matinas">
-          <p>{title}</p>
-          <caption>Matina</caption>
-        </a>
-      )}
 
-      {hour >= 3 && hour < 6 && (
-        <a href="/devocionario/oficio/lauds">
-          <p>{title}</p>
-          <caption>Lauds</caption>
-        </a>
-      )}
+  if (hour >= 0 && hour < 3) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/matinas"
+        caption="Matina"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 6 && hour < 9 && (
-        <a href="/devocionario/oficio/prima">
-          <p>{title}</p>
-          <caption>Prima</caption>
-        </a>
-      )}
+  if (hour >= 3 && hour < 6) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/lauds"
+        caption="Lauds"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 9 && hour < 12 && (
-        <a href="/devocionario/oficio/terca">
-          <p>{title}</p>
-          <caption>Terça</caption>
-        </a>
-      )}
+  if (hour >= 6 && hour < 9) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/prima"
+        caption="Prima"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 12 && hour < 15 && (
-        <a href="/devocionario/oficio/sexta">
-          <p>{title}</p>
-          <caption>Sexta</caption>
-        </a>
-      )}
+  if (hour >= 9 && hour < 12) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/terca"
+        caption="Terça"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 15 && hour < 18 && (
-        <a href="/devocionario/oficio/noa">
-          <p>{title}</p>
-          <caption>Noa</caption>
-        </a>
-      )}
+  if (hour >= 12 && hour < 15) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/sexta"
+        caption="Sexta"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 18 && hour < 21 && (
-        <a href="/devocionario/oficio/vesperas">
-          <p>{title}</p>
-          <caption>Vésperas</caption>
-        </a>
-      )}
+  if (hour >= 15 && hour < 18) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/noa"
+        caption="Noa"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 
-      {hour >= 21 && hour < 0 && (
-        <a href="/devocionario/oficio/completas">
-          <p>{title}</p>
-          <caption>Completas</caption>
-        </a>
-      )}
-    </>
-  );
+  if (hour >= 18 && hour < 21) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/vesperas"
+        caption="Vésperas"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
+
+  if (hour >= 21 || hour < 0) {
+    return (
+      <LinkCard
+        link="/devocionario/oficio/completas"
+        caption="Completas"
+        title="Pequeno Ofício de Nossa Senhora"
+      />
+    );
+  }
 }
