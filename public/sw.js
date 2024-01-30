3ebb40b6149b9c5dd56a34cfa85c4126a48e9932
ofrades <ofrades@pm.me>
2024-01-30 18:39:54 +0000

    clear old cache

--- public/sw.js
+++ public/sw.js
@@ -30,6 +30,15 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
     generateMonthlyApiUrls().then(() => {
       return self.clients.claim();
     }),
