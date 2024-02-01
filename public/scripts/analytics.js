4343e84009303803eebda00d1591fe4f43bbd35c
ofrades <ofrades@pm.me>
2024-02-01 18:22:11 +0000

    set scripts in config

---
+++ public/scripts/analytics.js
@@ -0,0 +1,4 @@
+window.dataLayer = window.dataLayer || [];
+
+dataLayer.push({ js: new Date() });
+dataLayer.push({ config: "UA-146185260-1" });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,12 +7,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.red,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.zinc,
-      },
       fontFamily: {
         display: ['"Berkshire Swash"', ...defaultTheme.fontFamily.serif],
         body: ['"Cardo"', ...defaultTheme.fontFamily.serif],
