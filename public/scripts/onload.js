5040ef31f16ccc11aa286ce11b404c6b6fe98167
ofrades <ofrades@pm.me>
2024-02-03 22:23:50 +0000

    add background sync

--- public/scripts/onload.js
+++ public/scripts/onload.js
@@ -1,10 +1,28 @@
 if ("function" === typeof importScripts) {
   importScripts(
+    "https://www.gstatic.com/firebasejs/10.1.0/firebase-app-compat.js",
+  );
+  importScripts(
     "https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js",
   );
+
+  const { initializeApp } = firebase;
+
+  const firebaseConfig = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+
   const { getMessaging } = firebase.messaging;
 
-  const messaging = getMessaging(firebaseApp);
+  const messaging = getMessaging(app);
 
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
