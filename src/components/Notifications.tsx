b092b6150f62d8b3f74a63c62f68ae6256ea8d8b
ofrades <ofrades@pm.me>
2024-02-05 17:14:48 +0000

    handle notifications better

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,29 +1,30 @@
 import { Icon } from "@iconify-icon/react";
+import { useState } from "react";
 
 export default function Notifications() {
+  const [notifications, setNotifications] = useState(Notification.permission);
+
   const requestPermission = async () => {
-    if (!("Notification" in window)) {
-      throw new Error("Notification not supported");
-    }
     const permission = await window.Notification.requestPermission();
-    if (permission !== "granted") {
-      throw new Error("Permission not granted for Notification");
-    }
+    setNotifications(permission);
   };
 
-  if (window && "Notification" in window) {
-    const permissionStatus = Notification.permission;
-
-    if (permissionStatus !== "granted") {
-      return (
-        <button
-          type="button"
-          className="flex cursor-pointer items-center bg-transparent gap-2"
-          onClick={() => requestPermission()}
-        >
-          <Icon icon="heroicons:bell" className="w-5 h-5" />
-        </button>
-      );
-    }
+  if (notifications !== "granted") {
+    return (
+      <button
+        type="button"
+        className="flex cursor-pointer items-center bg-transparent gap-2"
+        onClick={() => requestPermission()}
+      >
+        <Icon icon="heroicons:bell" className="w-5 h-5" />
+      </button>
+    );
   }
+  return (
+    <div className="flex gap-2 items-center">
+      Reset
+      <Icon icon="heroicons:bell-slash" className="w-5 h-5" />
+      in the browser settings
+    </div>
+  );
 }
