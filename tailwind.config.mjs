f3f33891e6d292111ced3e97d8a08878ddf28d93
Miguel Bastos <ofrades@pm.me>
2024-02-09 16:52:42 +0000

    Test new colors (#94)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,22 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        accent: colors.red,
+        gray: colors.sepia,
+        sepia: {
+          900: "#1d2021",
+          800: "#32302f",
+          700: "#504945",
+          600: "#7c6f64",
+          500: "#bfae98",
+          400: "#cba57e",
+          300: "#e2c1a2",
+          200: "#f2ddc7",
+          100: "#eee6d9",
+          50: "#f4eadd",
+        },
+      },
       fontFamily: {
         display: ['"Berkshire Swash"', ...defaultTheme.fontFamily.serif],
         body: ['"Cardo"', ...defaultTheme.fontFamily.serif],
