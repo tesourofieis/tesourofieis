8fd5106f50c1e6e4c82acc761f81aa6512908f19
ofrades <ofrades@pm.me>
2024-02-12 15:52:35 +0000

    consolidate responses

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("meta is correct", async ({ page }) => {
+test("title is correct", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
