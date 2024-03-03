3ac2911462c426a226280df86e942c4602e7dfab
ofrades <ofrades@pm.me>
2024-03-03 23:01:29 +0000

    fix ordo

--- src/components/Proper.tsx
+++ src/components/Proper.tsx
@@ -3,7 +3,7 @@ import { yyyyMMDD, type ProperDay } from "../lib/utils";
 import Loading from "./Loading";
 
 export default function Proper({ section }: { section?: string }) {
-  const [proper, setProper] = useState<ProperDay>();
+  const [proper, setProper] = useState<ProperDay[]>();
 
   const date = yyyyMMDD(new Date());
 
@@ -20,7 +20,7 @@ export default function Proper({ section }: { section?: string }) {
     fetchData();
   }, []);
 
-  if (!proper)
+  if (!proper?.length)
     return (
       <div className="mt-2 flex flex-col justify-center items-center">
         <p> A gerar...</p>
@@ -29,46 +29,50 @@ export default function Proper({ section }: { section?: string }) {
     );
 
   if (!section) {
-    return <h2>{proper?.info.title}</h2>;
+    return <h2>{proper[0].info.title}</h2>;
   }
 
   return (
     <>
-      {proper?.sections
-        .filter(({ id }) => id === section)
-        .map((section) => (
-          <div key={section?.id}>
-            <h2
-              className="text-center text-red-500"
-              id={section?.id.toLowerCase()}
-            >
-              {section?.id}
-            </h2>
-            <p className="text-center">{proper?.info.title}</p>
-            <div className="side-by-side not-content">
-              <span className="">
-                {section?.body?.latin?.map((text, i) => (
-                  <p
-                    key={`latin-${section.id}-${i}`}
-                    className="text-justify my-2"
-                  >
-                    {text}
-                  </p>
-                ))}
-              </span>
-              <span className="">
-                {section?.body?.vernacular?.map((text, i) => (
-                  <p
-                    key={`vernacular-${section.id}-${i}`}
-                    className="text-justify my-2"
-                  >
-                    {text}
-                  </p>
-                ))}
-              </span>
-            </div>
-          </div>
-        ))}
+      {proper.map((sections, index) => (
+        <div key={index}>
+          {sections.sections
+            .filter(({ id }) => id === section)
+            .map((section, sectionIndex) => (
+              <div key={section?.id}>
+                <h2
+                  className="text-center text-red-500"
+                  id={section?.id.toLowerCase()}
+                >
+                  {section?.id}
+                </h2>
+                <p className="text-center">{proper[0].info.title}</p>
+                <div className="side-by-side not-content">
+                  <span className="">
+                    {section?.body?.latin?.map((text, i) => (
+                      <p
+                        key={`latin-${section.id}-${i}`}
+                        className="text-justify my-2"
+                      >
+                        {text}
+                      </p>
+                    ))}
+                  </span>
+                  <span className="">
+                    {section?.body?.vernacular?.map((text, i) => (
+                      <p
+                        key={`vernacular-${section.id}-${i}`}
+                        className="text-justify my-2"
+                      >
+                        {text}
+                      </p>
+                    ))}
+                  </span>
+                </div>
+              </div>
+            ))}
+        </div>
+      ))}
     </>
   );
 }
