4a1ab6d08d3fbb067b005537314a6fafb29461e5
ofrades <ofrades@pm.me>
2024-03-15 15:42:45 +0000

    split

--- playwright-tests/index.spec.ts
+++ playwright-tests/index.spec.ts
@@ -1,8 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("navigation", async ({ page }) => {
-  test.slow();
-
+test("first page", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
@@ -11,16 +9,31 @@ test("navigation", async ({ page }) => {
   await page.getByRole("heading", { name: "Dia e Hora" }).click();
   await page.getByText("Muda a Missa, o Ofício, o").click();
   await page.getByLabel("add-notifications").click();
+});
+
+test("day", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
+
   await page.getByRole("link", { name: "Dia Variável" }).click();
   await page.getByRole("heading", { name: "Missa do Dia" }).click();
   await page.getByRole("heading", { name: "Introitus" }).click();
   await page.locator(".w-0 > .fixed").click();
   await page.locator(".w-48 > .fixed").click();
-  await page.getByRole("link", { name: "Tesouro dos Fiéis" }).click();
+});
+test("rosary", async ({ page }) => {
+  await page.goto("/");
+
   await page.getByRole("link", { name: "Rosario" }).click();
   await page.getByRole("heading", { name: "Rosário", exact: true }).click();
   await page.getByRole("link", { name: "Ave Maria" }).click();
   await page.getByText("℣. Ave, María, grátia plena,").click();
+});
+
+test("missal", async ({ page }) => {
+  await page.goto("/");
+
   await page
     .locator("summary")
     .filter({ hasText: "Advento" })
