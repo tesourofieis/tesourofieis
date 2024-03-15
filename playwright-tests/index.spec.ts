c6dbe4f10bc451beeb15387e149eea60dec0c683
ofrades <ofrades@pm.me>
2024-03-15 23:03:24 +0000

    simplify

--- playwright-tests/index.spec.ts
+++ playwright-tests/index.spec.ts
@@ -1,45 +1,38 @@
 import { expect, test } from "@playwright/test";
 
 test("first page", async ({ page }) => {
-  test.slow();
-
   await page.goto("/");
 
   await expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
 
-  await page.getByText("Em Portugal se conservará").click();
-  await page.getByRole("heading", { name: "Dia e Hora" }).click();
-  await page.getByText("Muda a Missa, o Ofício, o").click();
-  await page.getByLabel("add-notifications").click();
+  page.getByText("Em Portugal se conservará");
+  page.getByRole("heading", { name: "Dia e Hora" });
+  page.getByText("Muda a Missa, o Ofício, o");
+  page.getByLabel("add-notifications");
 });
 
 test("day", async ({ page }) => {
-  test.slow();
-
   await page.goto("/");
 
-  await expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
+  expect(page).toHaveTitle("Início | Tesouro dos Fiéis");
 
   await page.getByRole("link", { name: "Dia Variável" }).click();
-  await page.getByRole("heading", { name: "Missa do Dia" }).click();
-  await page.getByRole("heading", { name: "Introitus" }).click();
+  page.getByRole("heading", { name: "Missa do Dia" });
+  page.getByRole("heading", { name: "Introitus" });
   await page.locator(".w-0 > .fixed").click();
   await page.locator(".w-48 > .fixed").click();
 });
-test("rosary", async ({ page }) => {
-  test.slow();
 
+test("rosary", async ({ page }) => {
   await page.goto("/");
 
   await page.getByRole("link", { name: "Rosario" }).click();
-  await page.getByRole("heading", { name: "Rosário", exact: true }).click();
-  await page.getByRole("link", { name: "Ave Maria" }).click();
-  await page.getByText("℣. Ave, María, grátia plena,").click();
+  page.getByRole("heading", { name: "Rosário", exact: true });
+  page.getByRole("link", { name: "Ave Maria" });
+  page.getByText("℣. Ave, María, grátia plena,");
 });
 
 test("missal", async ({ page }) => {
-  test.slow();
-
   await page.goto("/");
 
   await page
@@ -52,18 +45,12 @@ test("missal", async ({ page }) => {
     .getByRole("main")
     .getByRole("link", { name: "Primeiro Domingo do Advento", exact: true })
     .click();
-  await page
-    .getByText(
-      "Ad te levávi ánimam meam: Deus meus, in te confíde, non erubéscam: neque irrí",
-    )
-    .click();
+  page.getByText(
+    "Ad te levávi ánimam meam: Deus meus, in te confíde, non erubéscam: neque irrí",
+  );
 });
 
 test("search", async ({ page }) => {
-  await page.goto("/");
-
   await page.keyboard.press("/");
-  await page.getByText("A pesquisa está disponível").click();
-  await page.getByRole("dialog", { name: "Pesquisar" }).press("Escape");
+  page.getByText("A pesquisa está disponível");
 });
-
