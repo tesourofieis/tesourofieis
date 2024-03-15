bf028a4c9a05cd33cdd7d3fd2265f534b0e623f5
ofrades <ofrades@pm.me>
2024-03-15 22:04:28 +0000

    remove performance

--- src/tests/calendar/2025.test.ts
+++ src/tests/calendar/2025.test.ts
@@ -4,13 +4,8 @@ import { printAll } from "../../lib/utils";
 
 describe("calendar 2025", () => {
   test("should render the correct calendar", () => {
-    const t1 = performance.now();
     const calendar = getCalendar(2025);
-    const t2 = performance.now();
 
     expect(printAll(calendar)).toMatchSnapshot();
-
-    // how fast in millisiconds
-    expect(t2 - t1).toBeLessThan(70);
   });
 });
--- src/tests/day/01-22.test.ts
+++ src/tests/day/01-22.test.ts
@@ -2,16 +2,7 @@ import { expect, test, bench } from "vitest";
 import { getDay } from "../../lib/getDay";
 
 test("2024-01-22", () => {
-  const t1 = performance.now();
   const proper = getDay("2024-01-22");
-  const t2 = performance.now();
 
   expect(proper).toMatchSnapshot();
-
-  // how fast in millisiconds
-  expect(t2 - t1).toBeLessThan(80);
-});
-
-bench("2024-01-22", () => {
-  getDay("2024-01-22");
 });
