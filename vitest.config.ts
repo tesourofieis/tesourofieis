aa156c5b33f71c80bfca86e15145abaf61963a12
Miguel Bastos <ofrades@miguel-2.local>
2024-03-16 16:35:18 +0000

    run coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       provider: "v8",
       include: ["src/lib/**/*"],
       exclude: ["src/lib/resources"],
-      reporter: ["text", "text-summary", "json"],
+      reporter: ["json-summary"],
       all: true,
     },
   },
