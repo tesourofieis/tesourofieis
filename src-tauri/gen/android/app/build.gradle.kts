986bd9f9b8f202339cef07885fc2d896f68de859
ofrades <ofrades@pm.me>
2024-03-27 12:02:44 +0000

    add android gen

---
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("rust")
+}
+
+android {
+    compileSdk = 34
+  namespace = "com.tesourofieis.app"
+    defaultConfig {
+        manifestPlaceholders["usesCleartextTraffic"] = "false"
+        applicationId = "com.tesourofieis.app"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 3
+        versionName = "1.2"
+    }
+    buildTypes {
+        getByName("debug") {
+            manifestPlaceholders["usesCleartextTraffic"] = "true"
+            isDebuggable = true
+            isJniDebuggable = true
+            isMinifyEnabled = false
+            packaging {                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+                jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
+                jniLibs.keepDebugSymbols.add("*/x86/*.so")
+                jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
+            }
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                *fileTree(".") { include("**/*.pro") }
+                    .plus(getDefaultProguardFile("proguard-android-optimize.txt"))
+                    .toList().toTypedArray()
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+  buildToolsVersion = "34.0.0"
+  ndkVersion = "26.2.11394342"
+}
+
+rust {
+    rootDirRel = "../../../"
+}
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
+
+apply(from = "tauri.build.gradle.kts")
---
+++ src-tauri/gen/android/app/src/main/java/com/tesourofieis/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.tesourofieis.app
+
+class MainActivity : TauriActivity()
