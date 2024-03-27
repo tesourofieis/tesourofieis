1d8d8b678d9e758e0f0e231707a9c9e421cabc56
ofrades <ofrades@pm.me>
2024-03-27 15:08:49 +0000

    bump

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tesourofieis.app"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
     buildTypes {
         getByName("debug") {
