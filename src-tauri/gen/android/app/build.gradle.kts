2eb3497a90c706dda22d951f99eb968d0aea8dba
Miguel Bastos <ofrades@pm.me>
2024-04-25 14:53:44 +0100

    ready to publish (#147)

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tesourofieis.app"
         minSdk = 24
         targetSdk = 33
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 8
+        versionName = "1.7"
     }
     buildTypes {
         getByName("debug") {
