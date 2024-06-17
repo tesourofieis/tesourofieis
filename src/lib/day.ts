c17db743fa4cefd29a90d0b965190aa65f2359ce
ofrades <ofrades@pm.me>
2024-06-17 08:37:32 +0100

    WIP

--- src/lib/day.ts
+++ src/lib/day.ts
@@ -1,4 +1,19 @@
 import { UTCDate } from "@date-fns/utc";
+import { getDate, getDay, getMonth } from "date-fns";
+import {
+  COMMUNE_C_10A,
+  COMMUNE_C_10B,
+  COMMUNE_C_10C,
+  COMMUNE_C_10PASC,
+  COMMUNE_C_10T,
+  LANGUAGE,
+  PATTERN_POST_EPIPHANY_SUNDAY,
+  TABLE_OF_PRECEDENCE,
+  TEMPORA_PENT23_0,
+  TEMPORA_RANK_MAP,
+  TYPE_TEMPORA,
+  getTranslation,
+} from "./constants.ts";
 import {
   getYear,
   isSameDay,
@@ -23,8 +38,6 @@ import {
   TEMPORA_PENT01_0,
   TRACTUS,
 } from "./constants.ts";
-import { Observance } from "./observance.ts";
-import { type Proper, ProperConfig } from "./proper.ts";
 import { getCustomPreface, match, yyyyMMDD } from "./utils.ts";
 
 // """ Class used to keep `Observance` objects for particular days of Missal.
@@ -52,146 +65,6 @@ class Day {
     return this.tempora[0]?.title;
   }
 
-  getProper(calendar: Calendar) {
-    /**
-     * Get proper that is used in today Mass. If given day does not have a dedicated proper,
-     * use the one from the latest Sunday.
-     */
-    const celebrationPropers = this.calculateProper(calendar, this.celebration);
-
-    if (this.commemoration.length) {
-      const commemorationPropers = this.calculateProper(
-        calendar,
-        this.commemoration,
-      );
-      for (const celebrationProper of celebrationPropers) {
-        for (const i of [0, 1]) {
-          const proper = celebrationProper[i];
-          if (proper) {
-            const commemorations = commemorationPropers
-              .filter(
-                (commemorationProper) =>
-                  proper.id !== commemorationProper[i].id,
-              )
-              .map((cp) => cp[i]);
-            if (proper && commemorations.length) {
-              proper.addCommemorations(commemorations);
-            }
-          }
-        }
-      }
-    }
-
-    return celebrationPropers;
-  }
-
-  private calculateProper(calendar: Calendar, observances: Observance[]) {
-    if (observances.length && observances.every((i) => i.hasProper())) {
-      const result: Proper[][] = [];
-      for (const observance of observances) {
-        const interReadingsSection = this.inferInterReadingSection(observance);
-        const preface = getCustomPreface(observance, this.tempora[0]);
-        const properConfig = new ProperConfig(
-          preface,
-          false,
-          interReadingsSection,
-        );
-        const proper = observance.getProper(properConfig);
-        result.push(proper);
-      }
-      return result;
-    }
-
-    const inferredObservances = this.inferObservance(calendar);
-    const rank =
-      observances.length && !match(observances, FERIA)
-        ? observances[0].rank
-        : 4;
-    const preface = getCustomPreface(
-      observances.length && !match(observances, FERIA)
-        ? observances[0]
-        : inferredObservances,
-    );
-    const prefaceOrDefault = !preface ? preface : PREFATIO_COMMUNIS;
-    const config: ProperConfig = {
-      preface: prefaceOrDefault,
-      stripAlleluia: true,
-    };
-
-    const propers = inferredObservances.getProper(config);
-    if (propers) {
-      for (const proper of propers) {
-        proper.rank = rank;
-      }
-    }
-
-    return [propers];
-  }
-
-  private inferObservance(calendar: Calendar) {
-    let date = new UTCDate(this.date);
-
-    // No proper for this day, get one from the latest Sunday
-    if (!isSunday(date)) {
-      if (isSameYear(date, this.date)) {
-        date = previousSunday(date);
-      }
-    }
-
-    // No proper for this day, get from epiphany
-    if (isSameDay(date, new UTCDate(getYear(this.date), 0, 6))) {
-      date = new UTCDate(getYear(this.date), 0, 6);
-    }
-
-    // if previous sunday is in the previous calendar year
-    // grab the proper from the circuncision
-    if (!isSameYear(date, this.date)) {
-      date = new UTCDate(getYear(this.date), 0, 1);
-    }
-
-    const day = calendar.get(yyyyMMDD(date));
-
-    if (day?.celebration.length) {
-      // Handling exceptions
-      if (day?.celebration[0].id === TEMPORA_EPI1_0) {
-        // "Feast of the Holy Family" replaces "First Sunday after Epiphany"; use the latter in
-        // following days without the own proper
-        return new Observance(TEMPORA_EPI1_0A, yyyyMMDD(date));
-      }
-
-      if (day?.celebration[0].id === TEMPORA_PENT01_0) {
-        // "Trinity Sunday" replaces "1st Sunday after Pentecost"; use the latter in
-        // following days without the own proper
-        return new Observance(TEMPORA_PENT01_0, yyyyMMDD(date));
-      }
-
-      if (day?.celebration[0].id === TEMPORA_NAT2_0) {
-        // When the last Sunday is the feast of Holy Name, use proper from Octave of the Nativity
-        return new Observance(SANCTI_01_01, yyyyMMDD(date));
-      }
-      return day?.celebration[0];
-    }
-
-    if (day.tempora?.length) {
-      return day.tempora[0];
-    }
-
-    return day?.celebration[0];
-  }
-
-  private inferInterReadingSection(observance: Observance) {
-    if (observance.id in CUSTOM_INTER_READING_SECTIONS) {
-      return CUSTOM_INTER_READING_SECTIONS[observance.id];
-    }
-    if (match(this.tempora, PATTERN_EASTER)) {
-      return GRADUALE_PASCHAL;
-    }
-    if (match(this.tempora, [PATTERN_PRE_LENTEN, PATTERN_LENT])) {
-      return TRACTUS;
-    }
-    return GRADUALE;
-  }
-
   serialize() {
     const serialized = {};
     const containers = ["tempora", "celebration", "commemoration"];
@@ -206,4 +79,88 @@ class Day {
   }
 }
 
-export { Day };
+class Observance {
+  date: string;
+  flexibility: string;
+  name: string;
+  rank: number;
+  colors: string[];
+  id: string;
+  title: string;
+  priority: number | null;
+  weekday: number | null;
+
+  // """ Build an Observance out of identifier and calendar date
+  //
+  // :param observance_id: observance's identifier in format
+  //                <flexibility>:<identifier>:<rank>
+  // :type observance_id: string
+  // :param date_: specific date in which the observance is supposed
+  //             to be placed. For some Sancti days its rank (class)
+  //             depends on which calendar day they occur.
+  // :type date_: `date ` object
+  // """
+  constructor(observanceId: string, date_: string) {
+    const [flexibility, name, rank, color] = observanceId.split(":");
+    this.date = date_;
+    this.flexibility = flexibility;
+    this.name = name;
+    this.rank = this.calcRank(observanceId, Number.parseInt(rank, 10));
+    this.colors = Array.from(color);
+    this.id = `${this.flexibility}:${this.name}:${this.rank}:${color}`;
+    this.title = getTranslation(LANGUAGE).TITLES[observanceId];
+
+    // Determine the weekday attribute based on the type of observance
+    if (
+      flexibility === TYPE_TEMPORA &&
+      ![
+        COMMUNE_C_10A,
+        COMMUNE_C_10B,
+        COMMUNE_C_10C,
+        COMMUNE_C_10PASC,
+        COMMUNE_C_10T,
+      ].includes(observanceId)
+    ) {
+      if (name.replace(/^.*-(\d+).*$/, "$1") === "10-Dur") {
+        this.weekday = 0;
+      }
+      this.weekday = Number.parseInt(name.replace(/^.*-(\d+).*$/, "$1"), 10);
+    } else {
+      this.weekday = getDay(this.date);
+    }
+
+    this.priority = this.calcPriority();
+  }
+
+  private calcRank(observanceId: string, originalRank: number): number {
+    for (const caseItem of TEMPORA_RANK_MAP) {
+      if (
+        getMonth(this.date) === caseItem.month &&
+        getDate(this.date) === caseItem.day &&
+        new RegExp(caseItem.pattern).test(observanceId)
+      ) {
+        return caseItem.rank;
+      }
+    }
+    return originalRank;
+  }
+
+  private calcPriority() {
+    for (let priority = 0; priority < TABLE_OF_PRECEDENCE.length; priority++) {
+      if (this.id.match(TABLE_OF_PRECEDENCE[priority])) {
+        return priority;
+      }
+    }
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      rank: this.rank,
+      title: this.title,
+      colors: this.colors,
+    };
+  }
+}
+
+export { Day, Observance };
