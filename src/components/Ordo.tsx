7d3dc839c374a9c8678442ee58e4bc35456e8200
ofrades <ofrades@pm.me>
2024-06-20 01:32:33 +0100

    fix ordo

--- src/components/Ordo.tsx
+++ src/components/Ordo.tsx
@@ -1,12 +1,13 @@
-import { getYear } from "date-fns";
-import { getCalendar } from "../lib/getCalendar";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { getCalendarDay } from "../lib/getCalendar";
+import { yyyyMMDD } from "../lib/utils";
 import LinkCard from "./LinkCard";
 import Loading from "./Loading";
 
 export default function Ordo({ section }: { section: string }) {
-  const calendar = getCalendar(getYear(new Date()));
+  const calendar = getCalendarDay(yyyyMMDD(new Date()));
 
-  if (!calendar?.length)
+  if (!calendar)
     return (
       <div className="mt-2 flex flex-col justify-center items-center">
         <Loading />
@@ -14,11 +15,21 @@ export default function Ordo({ section }: { section: string }) {
     );
 
   return (
-    <LinkCard
-      link={`/${calendar.celebration[0]?.link}#${section}`}
-      title={calendar.celebration[0]?.title}
-      caption={calendar.commemoration[0]?.title}
-      description={calendar.tempora[0].title}
-    />
+    <div className="p-8 flex gap-5">
+      <Icon icon="mdi:arrow-right" />
+      <LinkCard
+        link={`/${
+          calendar.celebration.length
+            ? calendar.celebration[0].link
+            : calendar.commemoration[0].link
+        }#${section}`}
+        title={
+          calendar.celebration.length
+            ? calendar.celebration[0].title
+            : calendar.commemoration[0].title
+        }
+        description={section.toUpperCase()}
+      />
+    </div>
   );
 }
