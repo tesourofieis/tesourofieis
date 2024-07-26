a299023a11a621d1255c4a9dd08830cd58a5854f
ofrades <ofrades@pm.me>
2024-07-26 16:58:03 +0100

    add unsubribe

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,8 +1,9 @@
 import { Icon } from "@iconify-icon/react";
-import { getToken } from "firebase/messaging";
+import { deleteToken, getToken } from "firebase/messaging";
 
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
+import { useEffect, useState } from "react";
 
 const firebaseConfig = {
   projectId: "tesourofieis",
@@ -15,6 +16,25 @@ const firebaseConfig = {
 };
 
 export default function Notifications() {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is already subscribed when the component mounts
+    checkSubscriptionStatus();
+  }, []);
+
+  const checkSubscriptionStatus = async () => {
+    if ("Notification" in window && Notification.permission === "granted") {
+      const app = initializeApp(firebaseConfig);
+      const messaging = getMessaging(app);
+      const token = await getToken(messaging, {
+        vapidKey: "YOUR_VAPID_KEY",
+      });
+      if (token) {
+        setIsSubscribed(true);
+      }
+    }
+  };
   const requestPermission = async () => {
     if (!("Notification" in window)) {
       throw new Error("Notification not supported");
@@ -44,6 +64,8 @@ export default function Notifications() {
               data: { token, topic: "angelus" },
             }),
           });
+
+          setIsSubscribed(true);
         } else {
           console.log("No registration token available.");
         }
@@ -52,19 +74,51 @@ export default function Notifications() {
       }
     }
   };
+  const unsubscribe = async () => {
+    if (!("Notification" in window)) {
+      throw new Error("Notification not supported");
+    }
+    try {
+      const app = initializeApp(firebaseConfig);
+      const messaging = getMessaging(app);
+      const token = await getToken(messaging);
+      if (token) {
+        await deleteToken(messaging);
+        await fetch("/api/subscriptions", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            data: { token, topic: "angelus" },
+          }),
+        });
+        setIsSubscribed(false);
+        console.log("Unsubscribed successfully");
+      } else {
+        console.log("No token found to unsubscribe");
+      }
+      setIsSubscribed(false);
+      console.log("Unsubscribed successfully");
+    } catch (err) {
+      console.error("Error unsubscribing:", err);
+    }
+  };
 
   if (window && "Notification" in window) {
-    const permissionStatus = Notification.permission;
-
     return (
       <button
         type="button"
-        aria-label="add-notifications"
+        aria-label={isSubscribed ? "remove-notifications" : "add-notifications"}
         className="flex cursor-pointer items-center bg-transparent gap-2"
-        disabled={permissionStatus === "granted"}
-        onClick={() => requestPermission()}
+        onClick={isSubscribed ? unsubscribe : requestPermission}
       >
-        <Icon icon="heroicons:bell" className="w-5 h-5" />
+        <Icon
+          icon={isSubscribed ? "heroicons:bell-slash" : "heroicons:bell"}
+          className="w-5 h-5"
+        />
+
+        {isSubscribed ? "Remover subscrição" : "Subscrever"}
       </button>
     );
   }
--- src/pages/api/subscriptions.ts
+++ src/pages/api/subscriptions.ts
@@ -1,26 +1,16 @@
 import admin from "firebase-admin";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import fs from "fs";
 
 export const prerender = false;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Initialize Firebase Admin SDK (do this once in your app)
 if (!admin.apps.length) {
   try {
-    const serviceAccountPath = join(
-      __dirname,
-      "../../../tesourofieis-firebase-adminsdk-sebsl-336ca58ba4.json",
-    );
-    const serviceAccount = JSON.parse(
-      fs.readFileSync(serviceAccountPath, "utf8"),
-    );
-
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      credential: admin.credential.cert({
+        projectId: import.meta.env.PROJECT_ID,
+        privateKey: import.meta.env.PRIVATE_KEY_ID,
+        clientEmail: import.meta.env.CLIENT_EMAIL,
+      } as admin.ServiceAccount),
     });
     console.log("Firebase Admin SDK initialized successfully");
   } catch (error) {
@@ -41,6 +31,28 @@ export async function POST({ request }) {
     });
   } catch (error) {
     console.error("Error subscribing to topic:", error);
+    return new Response(
+      JSON.stringify({ success: false, error: error.message }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
+
+export async function DELETE({ request }) {
+  const { data } = await request.json();
+  try {
+    // Unsubscribe the token from the topic
+    await admin.messaging().unsubscribeFromTopic(data.token, data.topic);
+
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error unsubscribing from topic:", error);
     return new Response(
       JSON.stringify({ success: false, error: error.message }),
       {
