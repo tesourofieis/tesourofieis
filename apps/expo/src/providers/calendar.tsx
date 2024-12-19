7741632bb61f3a1fb83880652b607ff79d41db10
ofrades <ofrades@pm.me>
2024-12-19 00:05:47 +0000

    fix calendar

--- apps/expo/src/providers/calendar.tsx
+++ apps/expo/src/providers/calendar.tsx
@@ -6,58 +6,63 @@ import { addDays, getYear, isWithinInterval, parseISO } from "date-fns";
 import { type PropsWithChildren, createContext, useContext } from "react";
 import { ActivityIndicator, View } from "react-native";
 
-const CalendarContext = createContext<{
-  calendar: Day[];
-  day: Day;
-  mass: Mass[];
-  novenas?: Mass[];
-}>(undefined);
+const CalendarContext =
+  createContext<
+    (year?: number) => {
+      calendar: Day[];
+      day: Day;
+      mass: Mass[];
+      novenas?: Mass[];
+    }
+  >(undefined);
 
 export function CalendarProvider({ children }: PropsWithChildren) {
-  const date = new Date();
+  const getCalendarData = (year?: number) => {
+    const date = new Date();
+    const calendar = getCalendar(year ?? getYear(date));
+    const day = getCalendarDay(yyyyMMDD(date));
+    const mass = day?.mass || [];
 
-  const calendar = getCalendar(getYear(date));
-  const day = getCalendarDay(yyyyMMDD(date));
-  const mass = day.mass;
+    function getNovenas() {
+      const endDate = addDays(date, 9);
+      const novenaObservances: Mass[] = [];
 
-  function getNovenas() {
-    const endDate = addDays(date, 9);
-    const novenaObservances: Mass[] = [];
-
-    for (const day of calendar) {
-      const dayDate = parseISO(day.date);
-      if (isWithinInterval(dayDate, { start: date, end: endDate })) {
-        const novenas = day.mass
-          .filter((mass) => mass.novena)
-          .map((i) => ({ ...i, date: yyyyMMDD(dayDate) }));
-        novenaObservances.push(...novenas);
+      for (const day of calendar) {
+        const dayDate = parseISO(day.date);
+        if (isWithinInterval(dayDate, { start: date, end: endDate })) {
+          const novenas = day.mass
+            .filter((mass) => mass.novena)
+            .map((i) => ({ ...i, date: yyyyMMDD(dayDate) }));
+          novenaObservances.push(...novenas);
+        }
       }
+      return novenaObservances;
     }
 
-    return novenaObservances;
-  }
+    if (!calendar || !day) {
+      return null;
+    }
 
-  if (!calendar || !day) {
-    return (
-      <View className="flex-auto justify-center items-center bg-sepia-200 dark:bg-sepia-900">
-        <ActivityIndicator className="text-red-600" />
-      </View>
-    );
-  }
+    return { calendar, day, mass, novenas: getNovenas() };
+  };
 
   return (
-    <CalendarContext.Provider
-      value={{ mass, day, calendar, novenas: getNovenas() }}
-    >
+    <CalendarContext.Provider value={getCalendarData}>
       {children}
     </CalendarContext.Provider>
   );
 }
 
-export const useCalendar = () => {
-  const context = useContext(CalendarContext);
-  if (context === undefined) {
+export const useCalendar = (year?: number) => {
+  const getCalendarData = useContext(CalendarContext);
+  if (getCalendarData === undefined) {
     throw new Error("useCalendar must be used within a CalendarProvider");
   }
-  return context;
+
+  const calendarData = getCalendarData(year);
+  if (!calendarData) {
+    throw new Error("Failed to get calendar data");
+  }
+
+  return calendarData;
 };
