7dc4a43fd6950ff92067ca6a77c028a8db71513e
ofrades <ofrades@pm.me>
2025-01-19 17:59:41 +0000

    WIP add agostinho 0505

--- apps/expo/src/app/(tabs)/missal/santos/05-05-coimbra-lamego-leiria.tsx
+++ apps/expo/src/app/(tabs)/missal/santos/05-05-coimbra-lamego-leiria.tsx
@@ -2,6 +2,7 @@ import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import React from "react";
+import LinkCard from "~/components/LinkCard";
 
 export default function Page0505CoimbraLamegoLeiria() {
   return (
@@ -9,6 +10,322 @@ export default function Page0505CoimbraLamegoLeiria() {
       <ScrollView>
         <View className="docs">
           <Text className="h1">Conversão de S. Agostinho, a 5 de Maio</Text>
+
+          <Text className="h3">Intróito</Text>
+
+          <Text className="em">Is. 65, 16</Text>
+
+          <View className="side-by-side">
+            <Text className="text-base latin">
+              Venite, audíte, et narrábo, omnes qui timétis Deum, quanta fecit
+              ánimæ meæ miséricors et miserátor Dóminus, aleluia, aleluia.{" "}
+              <Text className="text-base latin">Ps. ibid., 1-2</Text> Jubiláte
+              Deo, omnis terra, psalmum dícite nómini ejus: date glóriam laudi
+              ejus. ℣. Gloria Patri...
+            </Text>
+            <Text className="text-base vernacular">
+              Vinde, escutai vós todos, os que temeis a Deus; e eu vos narrarei
+              as muitas graças que o Senhor misericordioso e compassivo fez à
+              minha alma, aleluia, aleluia.{" "}
+              <Text className="text-base vernacular">Sl. ibid., 1-2</Text> Povos
+              de toda a terra: cantai um hino ao seu nome: rendei-Lhe louvores e
+              honras. ℣. Glória ao Pai...
+            </Text>
+
+            <Text className="h3">Oração</Text>
+
+            <View className="side-by-side">
+              <Text className="text-base latin">
+                Deus, qui ineffábili providéntia beátum Augustínum ab errórum
+                ténebris ad lucem Evangélicæ veritátis aduxísti: da nobis,
+                quæsumus, ut qui ejus hódie Conversiónem cólimus, ejúsdem apud
+                te précibus actus nostros in mélius reformémus. Per Dóminum...
+              </Text>
+              <Text className="text-base vernacular">
+                Ó Deus, que com inefável providéncia trouxestes o bem-aventurado
+                Agostinho das trevas do erro à luz da verdade evangélicæ,
+                concedei, nós vos pedimos, que pelas preces daquele cuja
+                conversão hoje celebramos reformemos para melhor os nossos
+                actos. Por nosso Senhor...
+              </Text>
+            </View>
+
+            <Text className="h3">Epístola</Text>
+
+            <Text className="em">Rm. 13, 11-14</Text>
+
+            <View className="side-by-side">
+              <Text className="text-base latin">
+                Léctio Epístolæ beáti Pauli Apóstoli ad Romános.
+              </Text>
+              <Text className="text-base vernacular">
+                Lição da Ep.ª do B. Ap.º Paulo aos Romanos.
+              </Text>
+              <Text className="text-base latin">
+                Frates: Hora est jam nos de somno súrgere. Nunc enim própior est
+                nostra salus, quam cum credídimus. Nox præcéssit, dies autem
+                apropinquávit. Abjiciámus ergo ópera tenebrárum, et induámur
+                arma lucis. Sicut in die honéste ambulémus: non in
+                comessatiónibus, et ebrietátibus, non in cubílibus et
+                impudicítiis, non in contentióne, et æmulatióne: sed induímini
+                Dóminum Jesum Christum et carnis curam ne fecéritis in
+                desidériis.
+              </Text>
+              <Text className="text-base vernacular">
+                Já é chegada a hora de nos levantarmos do sono, porquanto está
+                mais próxima a nossa salvação do que quando recebemos a fé. A
+                noite passou e o dia se apróxima. Deixemos, portanto, as obras
+                das trevas e vistamos as armas da luz. Caminhemos como de dia,
+                honestamente, não em gulodice e em embriaguez, não em
+                desonestidades e dissoluções: mas revisto-vos do Senhor Jesus
+                Cristo e não tomeis cuidado da carne em vossas vontades.
+              </Text>
+            </View>
+
+            <View className="side-by-side">
+              <Text className="text-base latin">
+                Allelúja, allelúja. ℣. Dirupísti víncula mea, Deus meus: tibi
+                sacrificábo hóstiam laudis, et nomem Dómini invocábo. Allelúja.
+                ℣. Confitébor tibi, quóniam exaudísti me: et factus es mihi in
+                salútem. Allelúja.
+              </Text>
+              <Text className="text-base vernacular">
+                Aleluia, aleluia. ℣. Rompestes os meus laços, ó meu Deus, a vós
+                oferecerei o sacrifício de louvor, e invocarei o nome do Senhor.
+                Aleluia. ℣. Louvar-Vos-ei, porque me escutastes e me salvastes.
+                Aleluia.
+              </Text>
+            </View>
+
+            <Text className="h3">Sequência</Text>
+
+            <View className="side-by-side">
+              <Text className="text-base latin">
+                De profúndis tenebrárum{"\n"}
+                Mundo lumem exit clarum,{"\n"}
+                Et scientílat hódie.
+              </Text>
+              <Text className="text-base vernacular">
+                Do profundo das trevas{"\n"}
+                Surge ao mundo clara luz{"\n"}
+                Que ainda hoje refulge.
+              </Text>
+
+              <Text className="text-base latin">
+                Olim quidem vas erróris{"\n"}
+                Augustínus vas honóris,{"\n"}
+                Datus est Ecclésiæ.
+              </Text>
+              <Text className="text-base vernacular">
+                Fora outrora vaso de erro,{"\n"}
+                Mas depois como vaso de honra{"\n"}
+                Agostinho é dado à Igreja.
+              </Text>
+
+              <Text className="text-base latin">
+                Verbo Dei dum obédit,{"\n"}
+                Credit errans, et accédit,{"\n"}
+                Ad Baptísmi grátiam.
+              </Text>
+              <Text className="text-base vernacular">
+                Dando ouvidos à palavra de Deus,{"\n"}O que errava crê e recebe
+                {"\n"}A graça do baptismo.
+              </Text>
+
+              <Text className="text-base latin">
+                Quam in primis tuebátur,{"\n"}
+                Verbis, scriptis exsecrátur,{"\n"}
+                Erróris falláciam.
+              </Text>
+              <Text className="text-base vernacular">
+                Por palavra e pela pena{"\n"}
+                Detesta os erros falazes{"\n"}
+                Que primeiro defendera.
+              </Text>
+
+              <Text className="text-base latin">
+                Firmans fidem, formans mores{"\n"}
+                Legis sacræ perversóres{"\n"}
+                Verbi necat gládio.
+              </Text>
+              <Text className="text-base vernacular">
+                Animando a fé, formando os costumes,{"\n"}Extermina com a espada
+                da sua palavra
+                {"\n"}Os perversores da lei sagrada.
+              </Text>
+
+              <Text className="text-base latin">
+                Obmutéscit Fortunátus{"\n"}
+                Cedunt Manes et Donátus{"\n"}
+                Tantæ lucis rádio.
+              </Text>
+              <Text className="text-base vernacular">
+                Emudece Fortunato,{"\n"}
+                Cedem Manes e Donato{"\n"}
+                Ao brilho de tanta luz.
+              </Text>
+
+              <Text className="text-base latin">
+                Mundus marcens et inánis{"\n"}
+                Et doctrínis tritus vanis{"\n"}
+                Per pestem hæréticam:
+              </Text>
+              <Text className="text-base vernacular">
+                O mundo murcho e sem vida,{"\n"}E gasto por vãs doutrinas,{"\n"}
+                Pela peste da heresia:
+              </Text>
+
+              <Text className="text-base latin">
+                Multum cœpit fructum ferre,{"\n"}
+                Dum in fines orbis terræ{"\n"}
+                Fidem sparsit únicam.
+              </Text>
+              <Text className="text-base vernacular">
+                Começou a dar muito fruto,{"\n"}
+                Quando espalhou a fé única,{"\n"}
+                Até aos confins da terra.
+              </Text>
+
+              <Text className="text-base latin">
+                Clericális vitæ formam{"\n"}
+                Conquadrávit, juxta normam{"\n"}
+                Cœtus Apostólici.
+              </Text>
+              <Text className="text-base vernacular">
+                Determinou segundo o modelo{"\n"}
+                Do Colégio Apostólico{"\n"}A forma de vida clerical.
+              </Text>
+
+              <Text className="text-base latin">
+                Sui quippe nil habébant{"\n"}
+                Tamquam suum, sed vivébant{"\n"}
+                In commúni clérici.
+              </Text>
+              <Text className="text-base vernacular">
+                De si nada possuíam{"\n"}
+                Como próprio, mas viviam{"\n"}
+                Os clérigos em comum.
+              </Text>
+
+              <Text className="text-base latin">
+                Sic multórum pro salúte{"\n"}
+                Diu vivens in virtúte,{"\n"}
+                Tandem bona senectúte{"\n"}
+                Dormívit cum pátribus.
+              </Text>
+              <Text className="text-base vernacular">
+                Assim pela salvação de muitos{"\n"}
+                Após longo vida virtuosa,{"\n"}
+                Finalmente em santa velhice{"\n"}
+                Descansou com os maiores.
+              </Text>
+
+              <Text className="text-base latin">
+                In extrémis nil legávit,{"\n"}
+                Qui nil suum æstimávit,{"\n"}
+                Immo totum reputávit{"\n"}
+                Commúne cum frátribus.
+              </Text>
+              <Text className="text-base vernacular">
+                Ao morrer, nada legou{"\n"}O que nada seu julgou,{"\n"}
+                Antes tudo reputou{"\n"}
+                Comum a si e aos irmãos.
+              </Text>
+
+              <Text className="text-base latin">
+                Salve, gemma Confessórum,{"\n"}
+                Lingua Christi, vox cælórum,{"\n"}
+                Tuba vitæ, lux Doctórum,{"\n"}
+                Præsul beatíssime.
+              </Text>
+              <Text className="text-base vernacular">
+                Salve, joia dos Confessores,{"\n"}
+                Lingua de Cristo, voz do céu,{"\n"}
+                Tuba de vida, luz dos Doutores,{"\n"}Ó Chefe bem-aventurado.
+              </Text>
+
+              <Text className="text-base latin">
+                Qui te patrem venerántur,{"\n"}
+                Te ductóre consequántur{"\n"}
+                Vitam, in qua glorántur,{"\n"}
+                Beatórum ánimæ. Amen. Allelúja.
+              </Text>
+              <Text className="text-base vernacular">
+                Fazei que, sob a tua direcção,{"\n"}
+                Os que te veneram como pai,{"\n"}
+                Alcancem aquela vida,{"\n"}
+                Onde as almas dos justos são glorificados. Amen. Aleluia.
+              </Text>
+            </View>
+
+            <Text className="h3">Evangelho</Text>
+
+            <LinkCard
+              href="/missal/pentecostes/pent3-0#evangelho"
+              title="Terceiro Domingo depois de Pentecostes"
+            />
+          </View>
+
+          <Text className="h3">Ofertório</Text>
+
+          <Text className="em">Mt. 11, 28</Text>
+
+          <View className="side-by-side">
+            <Text className="text-base latin">
+              Veníte ad me omnes qui laborátis et oneráti estis, et ego refíciam
+              vos, allelúja.
+            </Text>
+            <Text className="text-base vernacular">
+              Vinde a mim todos os que trabalhais e estais sobrecarregados e eu
+              vos aliviarei, aleluia.
+            </Text>
+          </View>
+
+          <Text className="h3">Secreta</Text>
+
+          <View className="side-by-side">
+            <Text className="text-base latin">
+              Sancti Augustíni Confessóris tui atque Pontíficis précibus,
+              Dómine, plebis tuæ dona sanctífica: ut, quæ tibi tuo grata sunt
+              institúto, gratióra fiant patrocínio supplicántis. Per Dóminum...
+            </Text>
+            <Text className="text-base vernacular">
+              Santificai, Senhor, as ofertas do vosso povo pelas preces do vosso
+              Confessor e Pontífice Agostinho, a fim de que, sendo-vos já
+              agradáveis (porque por Vós foram instituídas), mais agradáveis
+              ainda se tornem pelo patrocínio do suplicante. Por nosso Senhor...
+            </Text>
+          </View>
+
+          <Text className="h3">Comúnio</Text>
+
+          <Text className="em">Lc. 15, 7</Text>
+
+          <View className="side-by-side">
+            <Text className="text-base latin">
+              Dico vobis, quod gáudium magnum erit in cælo super uno peccatóre
+              pœniténtiam agénte, allelúja.
+            </Text>
+            <Text className="text-base vernacular">
+              Digo-vos que haverá grande júbilo no céu por um pecador que faz
+              penitência, aleluia.
+            </Text>
+          </View>
+
+          <Text className="h3">Postcomúnio</Text>
+
+          <View className="side-by-side">
+            <Text className="text-base latin">
+              Sanctificáti, Dómine, salutári mystério, quæsumus: ut pro nobis
+              ejus non desit orátio, cujus nos donásti patrocínio gubernári. Per
+              Dóminum...
+            </Text>
+            <Text className="text-base vernacular">
+              Santificados, Senhor, pelo mystério salutar, nós Vos suplicamos
+              não cesse de interceder por nós aquele a cujo patrocínio nos
+              confiastes. Por nosso Senhor...
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
