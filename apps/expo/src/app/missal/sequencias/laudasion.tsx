b66fd9a977d09f2c31175592de9517d2d9e2f242
Miguel Bastos <ofrades@pm.me>
2025-06-13 00:26:15 +0100

    Fix sequencia (#188)

---
+++ apps/expo/src/app/missal/sequencias/laudasion.tsx
@@ -0,0 +1,220 @@
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import Language from "~/components/Language";
+
+export default function PageLaudaSion() {
+  return (
+    <SafeAreaView className="flex-1">
+      <ScrollView>
+        <View className="docs">
+          <Text className="h3">Sequência Corpo de Cristo</Text>
+          <Language>
+            <Text className="latin">
+              Láuda Síon Salvatórem, Láuda dúcem et pastórem, In hýmnis et
+              cánticis.
+            </Text>
+            <Text className="vernacular">
+              Louva, ó Sião, louva o teu Salvador! Louva com hinos e cânticos o
+              teu Principe e Pastor!
+            </Text>
+            <Text className="latin">
+              Quantum pótes, tantum áude: Quia májor ómni láude, Nec laudáre
+              súfficis.
+            </Text>
+            <Text className="vernacular">
+              Louva-O tanto quanto possas: nem tu podes louvá-l’O dignamente,
+              pois está acima de todos os louvores.
+            </Text>
+            <Text className="latin">
+              Láudis théma speciális, Pánis vívus et vitális Hódie propónitur.
+            </Text>
+            <Text className="vernacular">
+              O assunto especial dos teus louvores, hoje, é o Pão vivo e
+              vivificante:
+            </Text>
+            <Text className="latin">
+              Quem in sácræ ménsa coénæ Túrbæ frátrum duodénæ Dátum non
+              ambígitur.
+            </Text>
+            <Text className="vernacular">
+              É aquele mesmo Pão (nós o acreditamos) que foi dado ao grupo dos
+              Doze Irmãos na Última Ceia.
+            </Text>
+            <Text className="latin">
+              Sit laus pléna, sít sonóra, Sit jucúnda, sit decóra Méntis
+              jubilátio.
+            </Text>
+            <Text className="vernacular">
+              Que o louvor seja perene, sonoro e melodioso: que seja agradável e
+              belo, como a alegria que transporta as nossas almas!
+            </Text>
+            <Text className="latin">
+              Díes enim solémnis ágitur, In qua ménsæ príma recólitur Hújus
+              institútio.
+            </Text>
+            <Text className="vernacular">
+              Eis o dia solene, que nos recorda a primitiva instituição deste
+              Banquete divino.
+            </Text>
+            <Text className="latin">
+              In hac ménsa nóvi Régis, Nóvum Páscha nóvæ légis, Pháse vétus
+              términat.
+            </Text>
+            <Text className="vernacular">
+              Nesta mesa do novo Rei a Páscoa da nova Lei acaba com a páscoa
+              antiga.
+            </Text>
+            <Text className="latin">
+              Vetustátem nóvitas, Umbram fúgat véritas, Nóctem lux elíminat.
+            </Text>
+            <Text className="vernacular">
+              O rito antigo cede o lugar ao novo, como a imagem desaparece
+              diante da realidade, e a luz apaga a noite.
+            </Text>
+            <Text className="latin">
+              Quod in coéna Chrístus géssit, Faciéndum hoc expréssit In súi
+              memóriam.
+            </Text>
+            <Text className="vernacular">
+              Aquilo que Cristo praticou na Ceia, mandou que fizéssemos, também,
+              em sua memória.
+            </Text>
+            <Text className="latin">
+              Dócti sácris institútis, Pánem, vínum, in salútis Consecrámus
+              hóstiam.
+            </Text>
+            <Text className="vernacular">
+              E nós, instruídos pelo mandato divino, consagrámos o pão e o vinho
+              em hóstia de salvação.
+            </Text>
+            <Text className="latin">
+              Dógma dátur christiánis, Quod in cárnem tránsit pánis, Et vínum in
+              sánguinem.
+            </Text>
+            <Text className="vernacular">
+              É um dogma de fé para os cristãos que o pão passa para Carne de
+              Cristo e o vinho para seu Sangue.
+            </Text>
+            <Text className="latin">
+              Quod non cápis, quod non vídes, Animósa fírmat fídes, Præter rérum
+              órdinem.
+            </Text>
+            <Text className="vernacular">
+              Aquilo que não compreendeis, nem vedes, a fé viva o afirma sem
+              alterar a ordem da natureza.
+            </Text>
+            <Text className="latin">
+              Sub divérsis speciébus, Sígnis tantum, et non rébus, Látent res
+              exímiæ.
+            </Text>
+            <Text className="vernacular">
+              Debaixo de diversas espécies, distintas somente por sinais
+              exteriores, ocultam-se sublimes realidades.
+            </Text>
+            <Text className="vernacular">
+              Cáro cíbus, sánguis pótus: Mánet tamen Chrístus tótus, Sub utráque
+              spécie.
+            </Text>
+            <Text className="vernacular">
+              A Carne de Cristo é alimento, e o Sangue bebida; mas Ele existe
+              inteiro em cada uma das espécies.
+            </Text>
+            <Text className="latin">
+              A suménte non concísus, Non confráctus, non divísus: Integer
+              accípitur.
+            </Text>
+            <Text className="vernacular">
+              Quem O recebe, nem O parte, nem O corta, nem O divide; porém
+              recebe-O inteiro.
+            </Text>
+            <Text className="latin">
+              Súmit únus, súmunt mille: Quantum ísti, tantum ílle: Nec súmptus
+              consúmitur.
+            </Text>
+            <Text className="vernacular">
+              Quem O receba uma só pessoa, quer O recebam mil, todas recebem o
+              mesmo: recebem-n’O sem O consumirem.
+            </Text>
+            <Text className="latin">
+              Súmunt bóni, súmunt máli: Sórte tamen inæquáli, Vítæ vel
+              intéritus. Mors est mális, víta bónis:
+            </Text>
+            <Text className="vernacular">
+              Recebem-n’O bons e maus; porém com efeitos diferentes: uns
+              encontram vida; outros a morte! Para os maus é morte e para os
+              bons é vida.
+            </Text>
+            <Text className="latin">
+              Víde páris sumptiónis Quam sit díspar éxitus. Frácto demum
+              sacraménto,
+            </Text>
+            <Text className="vernacular">
+              Vede que diferentes são os efeitos que produz o mesmo alimento!
+            </Text>
+            <Text className="latin">
+              Ne vacílles, sed meménto Tantum ésse sub fragménto, Quantum tóto
+              tégitur.
+            </Text>
+            <Text className="vernacular">
+              Que a vossa fé não vacile quando a hóstia é dividida; mas
+              lembrai-vos de que Jesus tanto está no fragmento, como na hóstia
+              inteira.
+            </Text>
+            <Text className="latin">
+              Núlla réi fit scissúra: Sígni tantum fit fractúra, Qua nec státus,
+              nec statúra Signáti minúitur.
+            </Text>
+            <Text className="vernacular">
+              A substância não é dividida: somente o sinal é que é partido, mas
+              sem diminuição, nem no estado, nem na grandeza do que está sob as
+              espécies.
+            </Text>
+            <Text className="latin">
+              Ecce Pánis Angelórum, Fáctus cíbus viatórum:
+            </Text>
+            <Text className="vernacular">
+              Eis o Pão dos Anjos que se fez alimento dos homens viadores,
+            </Text>
+            <Text className="latin">
+              Vere pánis filiórum, Non mitténdus cánibus.
+            </Text>
+            <Text className="vernacular">
+              Verdadeiro pão dos inocentes, que não deve ser dado aos cães!
+            </Text>
+            <Text className="latin">
+              In figúris præsignátur, Cum Isáac immolátur, Agnus Páschæ
+              deputátur, Dátur mánna pátribus.
+            </Text>
+            <Text className="vernacular">
+              Antigamente foi representado por figuras: imolado com Isaque e
+              significado no cordeiro pascal e no maná do deserto.
+            </Text>
+            <Text className="latin">
+              Bóne pástor, pánis vére, Jésu, nóstri miserére: Tu nos pásce, nos
+              tuére, Tu nos bóna fac vidére In térra vivéntium.
+            </Text>
+            <Text className="vernacular">
+              Ó bom Pastor, ó Pão verdadeiro, ó Jesus, tende piedade de nós:
+              alimentai-nos, defendei-nos do mal e permiti que gozemos os
+              verdadeiros bens da terra dos vivos.
+            </Text>
+            <Text className="latin">
+              Tu qui cúncta scis et váles, Qui nos páscis hic mortáles: Túos ibi
+              commensáles, Coherédes et sodáles Fac sanctórum cívium.{" "}
+            </Text>
+            <Text className="vernacular">
+              Ó Vós, que tudo conheceis e podeis: ó Vós, que nos alimentais
+              nesta vida mortal, tornai-nos co-herdeiros e companheiros dos
+              habitantes da cidade celestial.{" "}
+            </Text>
+            <Text className="latin">
+              <Text className="response"> ℟. </Text> Amen.
+            </Text>
+            <Text className="vernacular">
+              <Text className="response"> ℟. </Text> Amen.
+            </Text>
+          </Language>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
