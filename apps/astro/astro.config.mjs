995cc35765c61e827e6b01b7091bd3dd35c2440d
Miguel Bastos <ofrades@miguel-2.local>
2025-07-06 21:53:06 +0100

    WIP remove partytown

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,9 +1,7 @@
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 import robotsTxt from "astro-robots-txt";
@@ -168,11 +166,6 @@ export default defineConfig({
     react(),
     sitemap(),
     robotsTxt(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
   markdown: {
     rehypePlugins: [rehypeHeadingIds],
