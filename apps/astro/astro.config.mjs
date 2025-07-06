6c13c58a586768fcfda95268cf4bb5392305134c
Miguel Bastos <ofrades@miguel-2.local>
2025-07-06 21:42:15 +0100

    WIP fix gtags for astro

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -122,22 +122,16 @@ export default defineConfig({
         {
           tag: "script",
           attrs: {
-            type: "text/partytown",
             src: "https://www.googletagmanager.com/gtag/js?id=G-CYLKZM1NJQ",
-            defer: true,
+            async: true,
           },
         },
         {
           tag: "script",
-          attrs: {
-            type: "text/partytown",
-            defer: true,
-          },
           content: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-          
             gtag('config', 'G-CYLKZM1NJQ');`,
         },
       ],
