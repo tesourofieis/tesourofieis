122598dd586bdaae3a216fae3230d31705a52004
ofrades <ofrades@pm.me>
2026-01-03 17:31:26 +0000

    bump

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   platforms: ["android", "web", "ios"],
   githubUrl: "https://github.com/tesourofieis/tesourofieis",
-  version: "1.1.0",
+  version: "1.2.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   userInterfaceStyle: "automatic",
